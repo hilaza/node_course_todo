27fed9b4f2c358ee58faadf25ce2e1bd004d9976
hilasmirnov <bugiza@gmail.com>
2016-12-19 21:28:30 +0200

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// var user = {
+//         name: 'Andy',
+//         age: 36,
+//         location: 'Tel Aviv'
+//     };
+// var {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    
+    // db.collection('Users').insertOne({
+    //     name: 'Andy',
+    //     age: 36,
+    //     location: 'Tel Aviv'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('unable to insert user', err);
+    //     }
+    //     // console.log(JSON.stringify(result.ops, undefined, 2));
+
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
+
+    db.close();
+});
