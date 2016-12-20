d235f380a3cc21c1b5203da98266c705bb725b48
hilasmirnov <bugiza@gmail.com>
2016-12-20 23:23:04 +0200

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,24 @@ UserSchema.statics.findByToken = function (token) {
     })
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (e, r) => {
+                if (e || !r) {
+                    return reject();
+                }
+                resolve(user);
+            });
+        });
+    });    
+};
+
 UserSchema.pre('save', function (next) {
     var user = this;
 
